a289c202c5a4d635a4270b7e6f6ec6255f34402d
asplundoo <114659197+asplundoo@users.noreply.github.com>
2024-05-08 23:07:03 +0200

    Prompt

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ characters = [("A", 0), ("B", 0), ("C", 0), ("D", 0), ("E", 0), ("F", 0), ("G", 
               ("!", 0), ("?", 0), ("#", 0), ("%", 0), ("$", 0), ("&", 0), ("/", 0), ("+", 0), ("_", 0)]
 
 
+def user(min, max):
+    while True:
+        try:
+            user_input = int(input())
+
+            if min <= user_input <= max:
+                return user_input
+
+        except ValueError:
+            continue
+
+
 
 def generate_password(allowed, length, letter_case=None):
     password = ""
